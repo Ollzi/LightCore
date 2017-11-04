45db8239fcca449573d0d9711ca2f949e5b44162
Olof <Oloflid@gmail.com>
2017-11-04 12:23:29 +0100

    Ported to .Net core

---
+++ LightCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LightCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
